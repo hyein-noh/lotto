f41450162906f1154b0590f08972116280f664b1
hyein-noh <nohyein82@gmail.com>
2020-01-30 18:51:46 +0900

    Add Templates

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', app.views.home, name="home"),
+    path('result/', app.views.result, name="result"),
+
+
 ]
