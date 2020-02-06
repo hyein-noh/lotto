494f7d0813189aef717f9a1c1f8a1c0b3c085856
hyein-noh <nohyein82@gmail.com>
2020-02-06 17:46:38 +0900

    d

--- project/settings.py
+++ project/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6wbua&&yiid%3-nnnck2o@n(*b32ae)$w!ci+i4n9d%1-+3)p2'
+# '6wbua&&yiid%3-nnnck2o@n(*b32ae)$w!ci+i4n9d%1-+3)p2'
+SECRET_KEY = os,environ.get('DJANGO_SECRET_KEY',  '6wbua&&yiid%3-nnnck2o@n(*b32ae)$w!ci+i4n9d%1-+3)p2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
